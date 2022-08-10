8bf2e48f66c78b18fbeb7081bb41162a20614c3d
2nikitinalexandr2 <2nikitinalexandr2@gmail.com>
2022-08-10 10:08:50 +0500

    скрипт выполняется без ошибок, но таблицы не создаются

--- src/db/connection.php
+++ src/db/connection.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace App\db\connection;
+use PDO;
 
 function createConnection()
 {
     $dbPath = __DIR__ . '/../../db.sqlite';
+    #$dbPath = __DIR__ . '../sqlite3_dbs/';
     touch($dbPath);
 
-    $db = null;
+    #$db = null;
+    $db=new PDO('sqlite:$dbPath');
 
+
+    
     //TODO: Create connection to Sqlite DB
 
     return $db;
--- src/db/initial.php
+++ src/db/initial.php
@@ -2,7 +2,30 @@
 
 namespace App\db\initial;
 
+global $db;
+
 function initializeDb($db)
 {
-    //TODO: Create initial tables
+  $statement1 = 'CREATE TABLE "users" (
+        "user_id"	INTEGER NOT NULL,
+        "email"	TEXT NOT NULL,
+        "first_name"	TEXT NOT NULL,
+        "last_name"	TEXT NOT NULL,
+        "password"	INTEGER NOT NULL,
+        "created_at"	NUMERIC NOT NULL,
+        PRIMARY KEY("user_id")
+      )';
+  $db->exec($statement1);
+
+  $statement2 =  'CREATE TABLE "post" (
+        "post_id"	INTEGER NOT NULL,
+        "title"	TEXT NOT NULL,
+        "body"	TEXT NOT NULL,
+        "creator_id"	INTEGER NOT NULL,
+        "post_created_at"	INTEGER NOT NULL,
+        PRIMARY KEY("post_id"),
+          FOREIGN KEY (creator_id)
+          REFERENCES users(user_id) ON UPDATE CASCADE
+                                               ON DELETE CASCADE)';
+  $db->exec($statement2);
 }
